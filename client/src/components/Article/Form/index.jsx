41689feee87ebfe1eb7b0c5b954bbb92c5b70a83
book-of-kells <60078020+book-of-kells@users.noreply.github.com>
2020-04-06 00:19:41 -0700

    Form now a function and can compare prevProps to props

--- client/src/components/Article/Form/index.jsx
+++ client/src/components/Article/Form/index.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
+
+
+function usePrevious(value) {
+  // The ref object is a generic container whose current property is mutable ...
+  // ... and can hold any value, similar to an instance property on a class
+  const ref = useRef();
+  
+  // Store current value in ref
+  useEffect(() => {
+      ref.current = value;  
+  }, [value]); // Only re-run if value changes
+  
+  // Return previous value (happens before update in useEffect above)
+  return ref.current;
+}
 
 /**
  * @props:
@@ -13,98 +28,82 @@ import { connect } from 'react-redux';
  *    body
  *    author
  */
-class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { title: '', body: '', author: '' }
-    /**
-     * handleSubmit.bind() creates an identical handleSubmit function
-     * that is bound to the specific Form component instance.
-     * Within the bound handleSubmit function, the `this` object can refer to properties of the 
-     * Form component instance, such as `this.props.onSubmitCreated` and `this.props.onSubmitEdited`.
-     */
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+function Form(props) {
+  const [formFields, setFormFields] = useState({ title: '', body: '', author: '' });
+  // Get the previous value (was passed into hook on last render)
+  const prevArticleToEdit = usePrevious(props.articleToEdit);
 
-  handleSubmit() {
+  const handleSubmit = () => {
     /*
-     this.props.onSubmitEdited will
-     1. dispatch 'SUBMIT_EDITED_ARTICLE' action to home reducer, which will
-     2. set the home reducer state 
-        a. state.home.articles set to all articles, including the updated article
-        b. state.home.articleToEdit set to undefined
-     3. map the home reducer state to component props 
-        a. Home function's props.articles set to state.home.articles 
-        b. Form component props.articleToEdit set to state.home.articleToEdit 
-     */
-    if(this.props.articleToEdit != undefined) {
-      axios.patch(`http://localhost:8000/api/articles/${this.props.articleToEdit._id}`, { ...this.state })
-        .then((res) => this.props.onSubmitEdited(res.data))
-        .then(() => this.setState({ title: '', body: '', author: '' }));
+    props.onSubmitEdited will
+    1. dispatch 'SUBMIT_EDITED_ARTICLE' action to home reducer, which will
+    2. set the home reducer state 
+      a. state.home.articles set to all articles, including the updated article
+      b. state.home.articleToEdit set to undefined
+    3. map the home reducer state to function props 
+      a. Home function props.articles set to state.home.articles 
+      b. Form function props.articleToEdit set to state.home.articleToEdit 
+    */  
+    if(props.articleToEdit != undefined) {
+      axios.patch(`http://localhost:8000/api/articles/${props.articleToEdit._id}`, { ...formFields })
+        .then((res) => props.onSubmitEdited(res.data))
+        .then(() => setFormFields({ title: '', body: '', author: '' }));
     } else {
     /*
-     this.props.onSubmitCreated will
+     props.onSubmitCreated will
      1. dispatch 'SUBMIT_CREATED_ARTICLE' action to home reducer, which will
      2. set the home reducer state to all articles, including the newly created article
-     3. map the home reducer state.articles to the Home function's props.articles
+     3. map the home reducer state.articles to the Home function props.articles
      */
-      axios.post('http://localhost:8000/api/articles', { ...this.state })
-        .then((res) => this.props.onSubmitCreated(res.data))
-        .then(() => this.setState({ title: '', body: '', author: '' }));
+      axios.post('http://localhost:8000/api/articles', { ...formFields })
+        .then((res) => props.onSubmitCreated(res.data))
+        .then(() => setFormFields({ title: '', body: '', author: '' }));
     }
   }
 
-  handleChangeField(key, event) {
-    this.setState({ [key]: event.target.value })
-  }
+  const handleChangeField = (key, event) => {
+    setFormFields({...formFields, [key]: event.target.value })
+  };
 
-  /** Re-setting state when props change used to be handled by
-  * 
-  * componentWillReceiveProps(nextProps) {
-  *   if(nextProps.articleToEdit != undefined) {
-  *     this.setState({ ...nextProps.articleToEdit })
-  *   }
-  * }
-  * But since componentWillReceiveProps is unsafe, changed it to componentDidUpdate.
+  /**
+  * Reset state when props change
   */
-  componentDidUpdate(prevProps, prevState) {
-    if(prevProps.articleToEdit == undefined && this.props.articleToEdit != undefined) {
-      this.setState({ 
-        title: this.props.articleToEdit.title,
-        body: this.props.articleToEdit.body,
-        author: this.props.articleToEdit.author
+  useEffect(() => {
+    if(prevArticleToEdit == undefined && props.articleToEdit != undefined) {
+      setFormFields({
+        title: props.articleToEdit.title,
+        body: props.articleToEdit.body,
+        author: props.articleToEdit.author        
       })
     }
-  }
+  }, [props.articleToEdit]);
 
-  render() {
-    return (
-      <div className="col-12 col-lg-6 offset-lg-3">
-        <input
-          onChange={(ev) => this.handleChangeField('title', ev)}
-          value={this.state.title}
-          className="form-control my-3"
-          placeholder="Article Title"
-        />
-        <textarea
-          onChange={(ev) => this.handleChangeField('body', ev)}
-          value={this.state.body}
-          className="form-control my-3"
-          placeholder="Article Body">
-        </textarea>
-        <input
-          onChange={(ev) => this.handleChangeField('author', ev)}
-          value={this.state.author}
-          className="form-control my-3"
-          placeholder="Article Author"
-        />
+  return (
+    <div className="col-12 col-lg-6 offset-lg-3">
+      <input
+        onChange={(ev) => handleChangeField('title', ev)}
+        value={formFields.title}
+        className="form-control my-3"
+        placeholder="Article Title"
+      />
+      <textarea
+        onChange={(ev) => handleChangeField('body', ev)}
+        value={formFields.body}
+        className="form-control my-3"
+        placeholder="Article Body">
+      </textarea>
+      <input
+        onChange={(ev) => handleChangeField('author', ev)}
+        value={formFields.author}
+        className="form-control my-3"
+        placeholder="Article Author"
+      />
 
-        <button onClick={this.handleSubmit} className="btn btn-primary float-right">
-          {this.props.articleToEdit ? 'Update' : 'Submit'}
-        </button>
-      </div>
-    )
-  }
+      <button onClick={handleSubmit} className="btn btn-primary float-right">
+        {props.articleToEdit ? 'Update' : 'Submit'}
+      </button>
+    </div>
+  )
 }
 
 /**
@@ -112,23 +111,22 @@ mapStateToProps
 @param state the DefaultRootState that contains the combined reducers' states under their names e.g. "home" for the home reducer's state
 @param ownProps? optional
 @returns object mapping the connected component's props as keys to values of the DefaultRootState
-  key: this (Home) component's props.articleToEdit 
+  key: Form function's props.articleToEdit 
   value: the home reducer's state.articleToEdit 
 */
 const mapStateToProps = (state) => ({
-  articleToEdit: state.home.articleToEdit,  // maps the home reducer's state.articleToEdit to this (Form) component's props.articleToEdit
+  articleToEdit: state.home.articleToEdit
 })
-
 /**
 mapDispatchToProps
 @param dispatch Store function that takes an Action (object representing "what changed" with a mandatory 'type' key) and returns home reducer's state
 @param ownProps? optional
 @returns mapping the connected component's prop functions as keys to values of dispatch functions that take Action parameters and return part of the home reducer's state
-  key: this (Form) component's CRUD functions (onSubmitCreated, onSubmitEdited)
+  key: Form function's CRUD functions (onSubmitCreated, onSubmitEdited)
   value: the home reducer's dispatch actions/functions that take Action parameters and return home reducer's state 
 */
 const mapDispatchToProps = (dispatch) => ({
-  onSubmitCreated: data => dispatch({ type: 'SUBMIT_CREATED_ARTICLE', data }), /* returns state `articles` => Home function's props */
+  onSubmitCreated: data => dispatch({ type: 'SUBMIT_CREATED_ARTICLE', data }), /* returns state `articles` => Home props */
   onSubmitEdited: data => dispatch({ type: 'SUBMIT_EDITED_ARTICLE', data }),    /* returns state `articles` => Home props, `articleToEdit` => Form props */
 });
 
